1d2317fe89a0e7930ccd68551e1c57f19310781a
kesh24 <keshnarana4@gmail.com>
2019-11-06 19:35:27 -0500

    heroku connected

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,4 @@
 const router = require("express").Router()
-const healthTrackerRoutes = require("./healthTracker");
 
-// routes
-router.use("/healthTracker", healthTrackerRoutes);
 
 module.exports = router;
