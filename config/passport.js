4ce60c20e1193da4bc76f9f244360d75490070bf
Saumya Wijesuriya <Saumyawijesuriya@yahoo.com>
2019-11-08 23:19:56 -0500

    adding passport.js for user login

---
+++ config/passport.js
@@ -0,0 +1,27 @@
+const JwtStrategy = require('passport-jwt').Strategy,
+  ExtractJwt = require('passport-jwt').ExtractJwt;
+
+
+const User = require('../models/User');
+const settings = require('../config/settings'); 
+
+
+module.exports = passport => {
+  let opts = {};
+  opts.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme('jwt');
+  opts.secretOrKey = settings.secret;
+  passport.use(
+    new JwtStrategy(opts, (jwt_payload, done) => {
+      User.findOne({ id: jwt_payload.id }, (err, user) => {
+        if (err) {
+          return done(err, false);
+        }
+        if (user) {
+          done(null, user);
+        } else {
+          done(null, false);
+        }
+      });
+    })
+  );
+};
