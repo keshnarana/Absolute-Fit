443364b3834b825c1dce042c7f677e4b094b8026
prashanti <iawwfte@gmail.com>
2019-11-10 17:35:58 -0500

    update sign up axios -2

--- client/src/Pages/Signup.js
+++ client/src/Pages/Signup.js
@@ -17,6 +17,59 @@ class Signup extends Component {
      };
     }
 
+    handleClickOpen = () => {
+      this.setState ({ open: true});
+    };
+
+    handleClose = () => {
+        this.setState ({ open: false });
+    };
+
+    handleSubmit = e => {
+        this.setState ({ [e.target.name]: e.target.value});
+    };
+
+    handleSubmit = e => {
+        e.preventDeafualt();
+
+        if (this.state.password === this.state.passwordConfrimation) {
+            let userDetails = {
+               firstName: this.state.firstName,
+               lastName: this.state.lastName,
+               userName: this.state.userName,
+               weight: this.state.weight,
+               password: this.state.password
+            } ;  
+            
+
+         axios.post('/auth/register', userDetails).then(result => {
+               this.props.history.push ('/login');
+
+         });
+
+        } else {
+           this.handleClickOpen();
+
+        }
+            };
+
+       render() {
+          retun (
+             <div>
+               <SignupCompent
+                handleChange={this.handleChange.bind()}
+                handleSubmit={this.handleSubmit}
+                open={this.state.handleClickOpen}
+               />
+             </div>
 
 
-} // signup comp
+         );
+
+       }
+
+
+    } // signup comp
+
+
+export default Signup;
