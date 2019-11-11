90eb780ecc262a62698e2d66e05907ee89c8788b
Amal Hussein <amalhussein@Amals-MBP.fios-router.home>
2019-11-11 11:43:40 -0500

    login contact

---
+++ serverAM.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const app = express();
+const PORT = process.env.PORT || 3001;
+const morgan = require('morgan');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors())
+
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/reacthealthtracker',
+  { useNewUrlParser: true }
+);
+const Users = require('./routes/Users')
+app.use('/users',Users)
+app.use(morgan('dev'));
+
+// Start the API server
+app.listen(PORT, function() {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
