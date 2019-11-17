68a1c3fd23f02670d3fbcb4f950827fd3e7a85d0
kesh24 <keshnarana4@gmail.com>
2019-11-17 16:35:57 -0500

    updating app

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import DashBoard from './pages/DashBoard';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
-
+import NutritionPage from './pages/NutritionPage';
+import ExercisePage from './pages/ExercisePage';
 
 
 import './App.css';
@@ -15,10 +15,12 @@ const App = () => (
     <div>
       <NavBar />
       <Switch>
-        <Route exact path="/" component={DashBoard} />
+      <Route exact path="/" component={DashBoard} />
         <Route exact path="/login" component={Login} />
         <Route exact path="/signup" component={Signup} />
-       
+        <Route exact path="/nutrition" component={NutritionPage} />
+        <Route exact path="/exercise" component={ExercisePage} />
+     
       </Switch>
     </div>
   </Router>
