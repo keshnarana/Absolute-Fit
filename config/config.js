47d026872b1a548a364ac052a4781655ab77b997
kesh24 <keshnarana4@gmail.com>
2019-12-01 12:44:26 -0500

    try

--- config/config.js
+++ config/config.js
@@ -2,7 +2,11 @@ require('dotenv').config()
 
 module.exports = {
     USER: process.env.EMAIL, 
+<<<<<<< HEAD
 
     PASS: process.env.PASSW
 
+=======
+    PASS: process.env.PASSW
+>>>>>>> parent of 520f867... done
 }
