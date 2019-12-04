a5c988722b8edc2d8a77a13f7d3c364c635444f0
keshnarana <50150544+keshnarana@users.noreply.github.com>
2019-12-03 19:21:44 -0500

    Update server.js

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.get("*", (req, res) => {
 // Connect to the Mongo DB
 mongoose.connect(
 
-  process.env.MONGODB_URI || 'mongodb://keshna:keshna1@ds241258.mlab.com:41258/heroku_f80n46qt',
+   'mongodb://keshna:keshna1@ds241258.mlab.com:41258/heroku_f80n46qt',
    { useNewUrlParser: true,useUnifiedTopology: true }
 );
 
